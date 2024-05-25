30089f7f61b4a6770d683c35bb999292a4b2db20
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-05-25 15:03:38 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,28 @@ function draw(){
     image(video, 0, 0, 400, 380);
     document.getElementById("status").innerHTML = "Status: Detecting objects";
     input = document.getElementById("request").innerHTML;
+    for(i = 0; i < objects.length; i++){
+        document.getElementById("object_count").innerHTML = objects.length + " objects found";
+
+        fill("#ff2222");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        
+        noFill();
+        stroke("#ff2222");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        if(objects[i].label == input){
+            video.stop();
+            objectDetector.detect(gotResults);
+            document.getElementById("status").innerHTML = "Object mentioned found";
+            speech = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance("Object mentioned found");
+            speech.speak(utterThis);
+        }
+        else{
+            document.getElementById("status").innerHTML = "Object mentioned not found";
+        }
+    }
 }
 
 function start(){
